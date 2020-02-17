5e1fe7c313973f682767170858fca77622e1647b
roger <roger@slixa.com>
2020-02-17 01:14:02 -0300

    Disable hud on death

--- src/client/hud_renderer.ts
+++ src/client/hud_renderer.ts
@@ -6,6 +6,7 @@ import { SpeedIndicator } from "./hud_renderers/speed_indicator";
 import { EnergyIndicator } from "./hud_renderers/energy_indicator";
 import { GunIndicator } from "./hud_renderers/gun_indicator";
 import { Radar } from "./hud_renderers/radar";
+import { NameRenderer } from "./hud_renderers/name_renderer";
 
 export class HudRenderer implements Renderable {
     private ctx: CanvasRenderingContext2D = null;
@@ -14,6 +15,9 @@ export class HudRenderer implements Renderable {
     private speedIndicator: SpeedIndicator;
     private gunIndicator: GunIndicator;
     private radar: Radar;
+    private nameRenderer: NameRenderer;
+
+    private alive = true;
 
     public constructor(private canvas: HTMLCanvasElement, private camera: Camera, private stage: Stage) {
         this.ctx = canvas.getContext('2d');
@@ -22,6 +26,7 @@ export class HudRenderer implements Renderable {
         this.speedIndicator = new SpeedIndicator(canvas, camera);
         this.gunIndicator = new GunIndicator(canvas, camera);
         this.radar = new Radar(canvas, camera, stage);
+        this.nameRenderer = new NameRenderer(canvas, camera, stage);
     }
 
     public update(info: ClientInfo) {
@@ -29,15 +34,21 @@ export class HudRenderer implements Renderable {
         this.speedIndicator.update(info);
         this.gunIndicator.update(info);
         this.radar.update(info);
+        this.nameRenderer.update(info);
+
+        this.alive = info.alive;
     }
 
     public render() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        if (!this.alive) return;
+
         this.energyIndicator.render();
         this.speedIndicator.render();
         this.gunIndicator.render();
         this.radar.render();
+        this.nameRenderer.render();
 
         return this.canvas;
     }
