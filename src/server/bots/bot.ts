6c79b6edd6413f24d7797500deb7b9e3e5ef82ce
SparK-Cruz <spark.crz@gmail.com>
2023-09-06 00:30:55 -0300

    Remove unused var in bot.ts

--- src/server/bots/bot.ts
+++ src/server/bots/bot.ts
@@ -11,7 +11,6 @@ import { BotsConfig, Config } from '../../space/config';
 const MIN_DISTANCE = 150;
 const MAX_DISTANCE = 400;
 const DISTANCE_BAND = 90;
-const IDEAL_BULLET_SPEED = 7;
 const ANGLE_TOLERANCE = 4;
 const ENERGY_RESERVE = 0.55;
 
